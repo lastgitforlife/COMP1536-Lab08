46e77db000eea0ae9581e4cb04cf893037cac46b
Garrett Tailby <lastgameforlife@hotmail.com>
2018-11-20 12:35:09 -0800

    Update ajax-app.js

--- ajax-app.js
+++ ajax-app.js
@@ -24,6 +24,45 @@ app.get("/ajax-GET", function (req, res) {
     res.send({msg: out});
 })
 
+app.get('/ajax-GET-list', function (req, res) {
+
+    //res.setHeader('Content-Type', 'application/json');
+    //console.log(req.query['format']);
+    let formatOfResponse = req.query['format'];
+    let dataList = null;
+
+    if(formatOfResponse == 'html-list') {
+
+        res.setHeader('Content-Type', 'text/html');
+        dataList = lists.getHTML();
+        res.send(dataList);
+
+    } else if(formatOfResponse == 'json-list') {
+
+        res.setHeader('Content-Type', 'application/json');
+        dataList = lists.getJSON();
+        res.send(dataList);
+
+    } else {
+        res.send({msg: 'Wrong format!'});
+    }
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+
+app.post("/post-form", function (req, res) {
+    res.setHeader("Content-Type", "application/json");
+    console.log("Stuff sent to server", req.body);
+    res.send(["you sent me:", req.body]);
+})
+
+//For page not found
+app.use(function (req, res, next) {
+    res.status(404).send("<html><head><title>Page not found</title></head>"
+    + "<body><p>Nothing here</p></body></html>");
+})
 
 //RUN SERVER
 let port = 8000;
