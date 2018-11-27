1ff0c5fcc1989897843e20493b77e2e8c0161caf
Garrett Tailby <lastgameforlife@hotmail.com>
2018-11-27 11:30:51 -0800

    Update data.js

--- core/data.js
+++ core/data.js
@@ -5,5 +5,20 @@
     {"character":"Link", "img":"/img/link.png"},
     {"character":"Samus Aran", "img":"/img/samusaran.png"},
     {"character":"Dark Samus", "img":"/img/darksamus.png"},
+    {"character":"Yoshi", "img":"/img/yoshi.png"},
+    {"character":"Kirby", "img":"/img/kirby.png"},
+    {"character":"Fox", "img":"/img/fox.png"},
+    {"character":"Pikachu", "img":"/img/pikachu.png"},
+    {"character":"Luigi", "img":"/img/luigi.png"},
+    {"character":"Ness", "img":"/img/ness.png"},
+    {"character":"Captain Falcon", "img":"/img/falcon.png"},
+    {"character":"Jiggly Puff", "img":"/img/jigglyPuff.png"},
+    {"character":"Peach", "img":"/img/peach.png"},
+    {"character":"Daisy", "img":"/img/daisy.png"},
+    {"character":"Bowser", "img":"/img/bowser.png"},
+    {"character":"Ice Climbers", "img":"/img/iceClimbers.png"},
+    {"character":"Sheik", "img":"/img/sheik.png"},
+    {"character":"Zelda", "img":"/img/zelda.png"},
+    {"character":"Dr. Mario", "img":"/img/drMario.png"}
 ]
 }
