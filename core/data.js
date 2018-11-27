44c29d351a0315a62dce380860422db0de529f20
Patrick <pkhwong.92@gmail.com>
2018-11-27 11:11:44 -0800

    Added JSON for Smash Characters
    Initialized JSON data for smash characters

---
+++ core/data.js
@@ -0,0 +1,9 @@
+{
+"smashCharacters":[
+    {"character":"Mario", "img":"/img/mario.png"},
+    {"character":"Donkey Kong", "img":"/img/donkeykong.png"},
+    {"character":"Link", "img":"/img/link.png"},
+    {"character":"Samus Aran", "img":"/img/samusaran.png"},
+    {"character":"Dark Samus", "img":"/img/darksamus.png"},
+]
+}
